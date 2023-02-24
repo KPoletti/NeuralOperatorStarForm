d3d5e281ec5942f9144410debfcc9a6e0d15d7c4
KPoletti <kepo6935@gmail.com>
2023-02-24 09:57:21 -0600

    Add explaination of functions

--- utils/utilsMain.py
+++ utils/utilsMain.py
@@ -8,19 +8,17 @@ from neuralop.utils import UnitGaussianNormalizer
 from neuralop.data.tensor_dataset import TensorDataset
 
 
-class parserInputParameters(argparse.Action):
-    def __call__(
-        self,
-        parser: argparse.ArgumentParser,
-        namespace: argparse.Namespace,
-        values,
-        option_string: str,
-    ):
-        with values as f:
-            parser.parse_args(f.read().split(), namespace)
-
-
 def dataSplit(params) -> tuple:
+    """
+    Finds the length of the time series and
+    splits it into training, testing, and validation
+    Input:
+        params: input parameters from the input file
+    Output:
+        trainSize: int length of the training data
+        testSize: int length of the testing data
+        validSize: int length of the validation data
+    """
     tmp = pd.read_hdf(params.TIME_PATH, "table").shape[0]
     trainSize = int(tmp * params.split[0])
     testSize = int(tmp * params.split[1])
@@ -31,20 +29,44 @@ def dataSplit(params) -> tuple:
 def loadData(params: dataclass, isDensity: bool) -> tuple:
     """
     Load data from the path specified in the input file
+    Input:
+        params: input parameters from the input file
+        isDensity: if True load in the density data,
+               if False load in the information about each time step
+    Output:
+        isDensity = True:
+
+            trainData: torch.tensor training data,
+            testingData: torch.tensor testing data,
+            validData: torch.tensor validation data
+                Each tensor has dimensions (N, S, S, 2)
+                N is the number of timesteps
+                S is the size of the image
+                2 corresponds to the density at time t and t+1
+        isDensity = False:
+            trainData: pd.DataFrame training data,
+            testingData: pd.DataFrame testing data,
+            validData: pd.DataFrame validation data,
+                each dataframe is split into df["t"] and df["t+1"]
+                the dataframe can be indexed by dimension and time step
+                Each df["t"] is a dataframe with the following columns:
+                "time" time in Myr,
+                "filename" name of the file corresponding to that data,
+                "number" the number of timesteps,
     """
     if isDensity:
         filename = params.TRAIN_PATH
         fullData = torch.load(filename)[:, :: params.sub, :: params.sub]
     else:
         filename = params.TIME_PATH
-        # TODO: Check formatting of time data to match the original data
+        # load in data about each time step
         fullData = pd.read_hdf(filename, "table")
     # find the indices to split the data
     trainSize, testSize, validSize = dataSplit(params)
     # load data
     trainData = fullData[:trainSize]
     testData = fullData[trainSize : trainSize + testSize]
-    validData = fullData[trainSize + testSize : trainSize + testSize + validSize]
+    validData = fullData[trainSize + testSize :]
     del fullData
     return trainData, testData, validData
 
@@ -54,6 +76,15 @@ def timestepSplit(
 ) -> tuple:
     """
     Split the data into the time step to predict and the time step to use
+    Input:
+        dataSet: torch.tensor data to split
+        dataInfo: pd.DataFrame information about each time step
+        params: input parameters from the input file
+    Output:
+        dataSet_a: torch.tensor time step to use
+        dataSet_u: torch.tensor time step to predict
+        dataInfo_a: pd.DataFrame information about the time step to use
+        dataInfo_u: pd.DataFrame information about the time step to predict
     """
     dataSet_a = dataSet[..., : params.T_in]
     dataSet_u = dataSet[..., params.T_in : params.T_in + params.T]
@@ -64,7 +95,22 @@ def timestepSplit(
 
 
 def prepareDataForTraining(params: dataclass, S: int) -> tuple:
-    """ """
+    """
+    Loads in the data,
+    Splits it into training, testing, and validation subsets
+    Splits the data into the time step to predict and the time step to use
+    Normalizes the data
+    Reformats the data into the correct shape for the neural network
+    Loads the data into pytorch dataLoaders
+    Input:
+        params: input parameters from the input file
+        S: int size of the image
+    Output:
+        trainLoader: torch.utils.data.DataLoader training data
+        testsLoader: torch.utils.data.DataLoader testing data
+        validLoader: torch.utils.data.DataLoader validation data
+        output_encoder: neuralop.utils.UnitGaussianNormalizer
+    """
     # TODO: Write tests for this function
 
     trainSize, testsSize, validSize = dataSplit(params)
