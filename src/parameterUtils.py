a0388962f6ea314650e55006a73a041b1b66cdeb
KPoletti <kepo6935@gmail.com>
2023-12-07 11:57:36 -0600

    Correct MLP Bug and refactor

--- src/parameterUtils.py
+++ src/parameterUtils.py
@@ -3,7 +3,54 @@ Help functions for converting between
 """
 import yaml
 from pathlib import Path
-from neuralop.training.losses import LpLoss, H1Loss
+import json
+from neuralop import LpLoss, H1Loss
+
+
+def convertParamsToDict(params_obj):
+    """
+    Converts the params to a dictionary for wandb
+    """
+    params_dict = {}
+    unneeded = [
+        "__name__",
+        "__doc__",
+        "__package__",
+        "__loader__",
+        "__spec__",
+        "__file__",
+        "__cached__",
+        "__builtins__",
+        "nn",
+        "sample_uniform_spherical_shell",
+        "linear_scale_dissipative_target",
+        "LpLoss",
+        "H1Loss",
+        "FieldwiseAggregatorLoss",
+        "loss_fn",
+        "losses",
+        "mapping",
+        "math",
+    ]
+    for key, value in vars(params_obj).items():
+        # skip the levels that are not needed
+        if key in unneeded:
+            continue
+        params_dict[key] = value
+    return params_dict
+
+
+def convertParamsToJSON(params):
+    """
+    Converts the params to a JSON for wandb
+    """
+    paramsDict = convertParamsToDict(params)
+    paramsJSON = json.dumps(
+        paramsDict, default=lambda o: o.__dict__, sort_keys=True, indent=4
+    )
+    # convert back to dictionary
+    paramsJSON = json.loads(paramsJSON)
+    return paramsJSON
 
 
 def set_Loss(params) -> LpLoss | H1Loss:
